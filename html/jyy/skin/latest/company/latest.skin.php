256901ff2f433e51fec327160aea33d58821030c
jiyy25 <jiyy825@gmail.com>
2024-01-10 19:36:37 +0900

    company게시판 최근게시스킨으로 게시판과 연동해서 처리함. 반응형 오류 수정완료.

--- html/jyy/skin/latest/company/latest.skin.php
+++ html/jyy/skin/latest/company/latest.skin.php
@@ -3,13 +3,16 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 include_once(G5_LIB_PATH.'/thumbnail.lib.php');
 
 // add_stylesheet('css 구문', 출력순서); 숫자가 작을 수록 먼저 출력됨
-add_stylesheet('<link rel="stylesheet" href="'.$latest_skin_url.'/style.css">', 0);
+
 $thumb_width = 925;
 $thumb_height = 450;
 $list_count = (is_array($list) && $list) ? count($list) : 0;
 ?>
-
-<div class="pic_lt">
+<div class="center-heading margin-top-40">
+		<h2><?php echo $bo_subject; ?></h2>
+</div>
+<div class="d-lg-flex justify-content-between jjy_picBox">
+    <div class="com_imgbox">
     <?php
     for ($i=0; $i<$list_count; $i++) {
     $thumb = get_list_thumbnail($bo_table, $list[$i]['wr_id'], $thumb_width, $thumb_height, false, true);
@@ -23,11 +26,16 @@ $list_count = (is_array($list) && $list) ? count($list) : 0;
     $img_content = '<img src="'.$img.'" alt="'.$thumb['alt'].'" >';
     $wr_href = get_pretty_url($bo_table, $list[$i]['wr_id']);
     ?>
-        <div class="gallery_li">
+        <div class="company_li">
             <a href="<?php echo $wr_href; ?>" class="lt_img"><?php echo run_replace('thumb_image_tag', $img_content, $thumb); ?></a>
-
         </div>
+        </div>
+    <div class="contentBox ml-4">
+    <?php echo $list[$i]['wr_content'] ;?>
+                <a href="<?php echo $list[$i]['wr_link1'] ;?>" class="p-2 border"><?php echo $list[$i]['wr_subject'] ;?></a>
+    </div>
     <?php }  ?>
-    </ul>
 
+
+    
 </div>
