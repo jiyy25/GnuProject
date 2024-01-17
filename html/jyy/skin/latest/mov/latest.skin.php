ab33eaefd77cdd4826958f39ccfb0ed35732f891
jiyy25 <jiyy825@gmail.com>
2024-01-17 15:39:06 +0900

    영상게시판 영상부분도 src의 주소부분을 변수처리하여  게시판으로 관리할 수 있도록 최근게시글로 연결함.  부트스트랩을 활용하여 반응형처리도 완료.

--- html/jyy/skin/latest/mov/latest.skin.php
+++ html/jyy/skin/latest/mov/latest.skin.php
@@ -11,31 +11,33 @@ $list_count = (is_array($list) && $list) ? count($list) : 0;
     <div class="center-heading margin-top-40">
 		<h2><?php echo $bo_subject; ?></h2>
 	</div>
-    <div class="back_img">
-    <div class="container d-flex justify-content-between align-items-center mv_all position-absolute">
+    <?php for($i=0; $i<$list_count; $i++){ 
+            $thumb = get_list_thumbnail($bo_table, $list[$i]['wr_id'], $thumb_width, $thumb_height, false, true);
+
+            if($thumb['src']) {
+                $img = $thumb['ori'];
+            } else {
+                $img = G5_IMG_URL.'/no_img.png';
+                $thumb['alt'] = '이미지가 없습니다.';
+            }
+            ?>
+    <div class="back_img py-3" style="background-image:url(<?php echo $img; ?>); ">
+    <?php } ?>
+    <div class="container d-lg-flex justify-content-between align-items-stretch mv_all py-3 my-5">
     <?php
     for ($i=0; $i<$list_count; $i++) {
-    $thumb = get_list_thumbnail($bo_table, $list[$i]['wr_id'], $thumb_width, $thumb_height, false, true);
-
-    if($thumb['src']) {
-        $img = $thumb['src'];
-    } else {
-        $img = G5_IMG_URL.'/no_img.png';
-        $thumb['alt'] = '이미지가 없습니다.';
-    }
-    $img_content = '<img src="'.$img.'" alt="'.$thumb['alt'].'" >';
     $wr_href = get_pretty_url($bo_table, $list[$i]['wr_id']);
     ?> 
-        <div class ="mv_texbox">
-        <a href="<?php echo $list[$i]['wr_link1']?>">
-        <?php echo $list[$i]['wr_content']?>
-        </a>
-
-        <a class="mv_btn text-center" href="<?php echo $list[$i]['wr_link1']?>">창업문의</a>
+        <div class ="mv_texbox col-lg-5 w-auto py-5 d-flex flex-column justify-content-between mb-3 mb-lg-0 ">
+            <a href="<?php echo $list[$i]['wr_link1']?>">
+            <?php echo $list[$i]['wr_content']?>
+            </a>
+            <a class="mv_btn text-center" href="<?php echo $list[$i]['wr_link1']?>">창업문의</a>
         </div>
-
-        <div>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/v61F2kOmb3E?si=uZskxr6evYwdh7ko" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <div class="flex-grow-1 pl-lg-5">
+            <div class="position-relative ml-lg-5 overflow-hidden" style="height:0; padding-top:56%; border-radius:10px; " >
+            <iframe class="position-absolute w-100 h-100" style="top:0; left:0;"   src="https://www.youtube.com/embed/<?php echo $list[$i]['wr_subject']; ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            </div>
         </div>
 
     <?php }  ?>
